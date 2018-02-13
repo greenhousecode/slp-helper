a920d22510c422aee21118f35adf79e9231d11ff
Rick Stevens <rick.stevens@greenhousegroup.com>
2018-02-13 09:41:51 +0100

    Standard style

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,34 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
   entry: './src/slp.js',
-  output: { filename: 'dist/slp.min.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'slp.min.js'
+  },
   module: {
     rules: [
       {
-        test: /.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader'
+      }
+    ]
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         screw_ie8: true,
         unused: true,
-        dead_code: true,
+        dead_code: true
       },
       mangle: {
-        screw_ie8: true,
+        screw_ie8: true
       },
       output: {
-        screw_ie8: true,
-      },
-    }),
-  ],
-};
+        screw_ie8: true
+      }
+    })
+  ]
+}
