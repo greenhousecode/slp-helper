2a6d870d412517a01be332c53d7cae6dc8502bfc
Rick Stevens <rick.stevens@greenhousegroup.com>
2018-02-13 09:58:11 +0100

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -8,27 +8,7 @@ module.exports = {
     filename: 'slp.min.js'
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader'
-      }
-    ]
+    rules: [{ loader: 'babel-loader' }]
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true,
-        unused: true,
-        dead_code: true
-      },
-      mangle: {
-        screw_ie8: true
-      },
-      output: {
-        screw_ie8: true
-      }
-    })
-  ]
+  plugins: [ new webpack.optimize.UglifyJsPlugin() ]
 }
